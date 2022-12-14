ff606aa156ad69fcff46bca05c2e353004d77f71
IgnacioSEO <112772324+IgnacioSEO@users.noreply.github.com>
2022-12-14 23:38:22 +0900

    ADD: MyBatis models에  추가/서버구동시 app crashed 발생

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const { createApp } = require("./app");
-const { appDataSource } = require("./src/models/dataSource");
-
+// const { appDataSource } = require("./src/models/dataSource");
+const { connection } = require("./src/models/db");
 const startServer = async () => {
   const app = createApp();
   const PORT = process.env.PORT;
 
-  await appDataSource.initialize();
+  await connection
+    .initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
+    .catch((err) => {
+      console.error("Error occurred during Data Source initialization", err);
+      database.destroy();
+    });
 
   app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);
---
+++ src/models/db.js
@@ -0,0 +1,35 @@
+const mysql = require("mysql2");
+
+const mybatisMapper = require("mybatis-mapper");
+
+const connection = mysql.createConnection({
+  host: process.env.MYBATIS_HOST,
+  user: process.env.MYBATIS_USERNAME,
+  database: process.env.MYBATIS_DATABASE,
+  password: process.env.MYBATIS_PASSWORD,
+});
+
+mybatisMapper.createMapper(["./src/models/mapper.xml"]);
+
+let param = {
+  test_id: 1,
+};
+
+//질의문 형식
+let format = { language: "sql", indent: "  " };
+let query = mybatisMapper.getStatement("testMapper", "testBasic", param, format);
+//첫번째는 xml의 name값, 두번째는 해당 xml의 id값, 세번째는 파라미터, 마지막은 포맷 입니다.
+
+console.log(query); //해당쿼리가 조합된 것을 볼 수 있습니다.
+
+connection.connect();
+connection.query(query, function (error, results, fields) {
+  //조회
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+});
+connection.end();
+
+module.exports = { connection };
